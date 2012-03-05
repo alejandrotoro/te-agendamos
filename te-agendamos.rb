3c5b8cf62b886f4be3f48652f60b6ab6f7191677
Alejandro Toro <alejandrotoro@espiral.co>
2012-03-05 14:57:14 -0500

    Overwriting styles and add contact form

--- te-agendamos.rb
+++ te-agendamos.rb
@@ -11,3 +11,38 @@ end
 get "/" do
   haml :index, { :layout => :'layouts/application' }
 end
+
+post "/contact" do
+  require 'pony'
+  require 'json'
+  content_type :json
+  
+  response = {:error => true}
+  response[:complete] = (!params[:email].empty? and !params[:comment].empty? and !params[:name].empty?)
+  
+  if response[:complete]
+    Pony.mail(
+      :from => "#{params[:name]} <#{params[:email]}>",
+      :to => "contacto@sietecumbres.com.co",
+      :subject => "#{params[:name]} ha enviado un mensaje de contacto.",
+      :body => params[:comment],
+      :port => "587",
+      :via => :smtp,
+      :via_options => {
+        :address              => "smtp.gmail.com",
+        :port                 => "587",
+        :enable_starttls_auto => true,
+        :user_name            => "contacto@sietecumbres.com.co",
+        :password             => "contacto7cumbres",
+        :authentication       => "plain",
+        :domain               => "sietecumbres.com.co"
+      }
+    )
+    response[:message] = "El mesaje se ha enviado con exito. Muchas gracias por contactarse con nosotros."
+    response[:error] = false
+  else
+    response[:message] = "Todos los campos deben ser diligenciados."
+  end
+  
+  response.to_json
+end
