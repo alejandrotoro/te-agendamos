a320813067ef3864cb732d44a55adff39e631b6a
Alejandro Toro <alejandro@hlmbusiness.com>
2012-03-11 19:05:51 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeAgendamos::Application.config.secret_token = 'fedb3404b4e585508c42b1d2ac494311c8961237dc2bf0b3a5ca96d066247ce7473c365405f0bc3dc591952c1e9949c08014f9547b0320b2af6801f498eef2c7'
