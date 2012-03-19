b80fd3737459d720d4b723227c8441f2736d9522
Alejandro Toro <alejandrotoro@espiral.co>
2012-03-18 19:33:32 -0500

    add user_root_path to ensure the redirection to dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,15 @@
 TeAgendamos::Application.routes.draw do
   resources :products
 
-  resources :providers
-
   match 'support' => 'supports#contact'
   match 'login_and_register' => 'users#login_and_register'
   match 'login' => 'users#login'
   match 'register' => 'users#register'
+  match 'dashboard' => 'users#dashboard'
   resources :categories
-
+  
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'users#dashboard', :as => :user_root
   end
   root :to => "home#index"
   devise_for :users
