d812f4047b14ad9b74d2b7c831929c8117890662
Alejandro Toro <alejandrotoro@espiral.co>
2012-05-06 17:05:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeAgendamosNew::Application.config.secret_token = '6847ef42283c649d1ac3733986d9ef0522aa9f8dc3f6402c5f55454fe95c6fb4b61ddf2d9d5caade7f1c074d8d1696922d7682d6711883c55a568124853fde79'
